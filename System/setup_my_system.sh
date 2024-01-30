13c773ddb008c6e497d33c41f252807c29f21e6d
Aditya <adityabohra619@gmail.com>
2024-01-30 16:42:53 +0530

    InI

---
+++ System/setup_my_system.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# For webp file thumbnail
+sudo apt install webp-pixbuf-loader
+
+sudo apt install ffmpegthumbnailer
+
+sudo apt install gstreamer1.0-libav
+
+rm -r ~/.cache/thumbnails/fail
+
+sudo apt-get install libheif1 heif-thumbnailer 
+
+
+# Add Docker Package
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+ sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# post installation 
+ sudo groupadd docker
+sudo usermod -aG docker $USER
+ newgrp docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
